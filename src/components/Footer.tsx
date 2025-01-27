6ce2c31b4c0bf4a183da00d2fb589d6bf05748bf
Kaylie Rim <kaylie.rim@gmail.com>
2025-01-27 15:09:16 -0500

    Style tablet view

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,36 +7,36 @@ import Text from "./Text";
 
 function Footer() {
   return (
-    <div className="bg-[#2F4156] mt-auto w-full h-16 rounded-t-[20px] flex items-center justify-between p-4">
-      <div className="flex gap-5">
+    <div className="bg-[#2F4156] mt-auto w-full h-16 rounded-t-[20px] flex items-center justify-between p-4 mobile:p-2">
+      <div className="flex gap-5 mobile:gap-3">
         <a
           href="mailto:rim.k@northeastern.edu"
           className="flex gap-3 cursor-pointer"
         >
-          <img src={EMAIL} className="w-6" />
+          <img src={EMAIL} className="w-5" />
         </a>
         <a
           href="https://github.com/kaylierim"
           target="_blank"
           className="flex gap-3 cursor-pointer"
         >
-          <img src={GITHUB} className="w-6" />
+          <img src={GITHUB} className="w-5" />
         </a>
         <a
           href="https://www.linkedin.com/in/kaylie-rim/"
           target="_blank"
           className="flex gap-3 cursor-pointer"
         >
-          <img src={LINKED_IN} className="w-6" />
+          <img src={LINKED_IN} className="w-5" />
         </a>
         <a href={resume} target="_blank" className="flex gap-3 cursor-pointer">
-          <img src={RESUME} className="w-6" />
+          <img src={RESUME} className="w-5" />
         </a>
       </div>
       <Text
         text="© 2025 Kaylie Rim. All rights reserved."
         type="Paragraph"
-        className="text-[#F5EFEB]"
+        className="text-[#F5EFEB] mobile:text-sm"
       />
     </div>
   );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -15,9 +15,28 @@ import Image from "@components/Image";
 import Card from "@components/Card";
 
 function Home() {
+  const skills1 = [
+    "Typescript",
+    "HTML",
+    "CSS",
+    "JavaScript",
+    "Python",
+    "Java",
+    "Kotlin",
+  ];
+
+  const skills2 = [
+    "Jira",
+    "Git/Github",
+    "Confluence",
+    "Figma",
+    "React",
+    "Tailwind CSS",
+    "ESLint",
+  ];
   return (
     <div className="w-full flex flex-col items-center gap-28 mb-28">
-      <div className="flex items-center gap-5">
+      <div className="flex items-center gap-5 px-8">
         <Image
           src={profilePhoto}
           alt="Kaylie's profile photo"
@@ -48,40 +67,36 @@ function Home() {
           </div>
         </div>
       </div>
-      <div className="flex gap-20 mobile:flex-col">
-        <div>
+      <div className="flex tablet:gap-10 tablet:flex-col mobile:px-4">
+        <div className="pr-10">
           <Text text="About Me" type="Heading2" />
           <Text
             text="I am actively searching for a Fall 2025 internship in the software field. I have been a front-end engineer co-op and a teaching assistant for Fundamentals of Computer Science 2. My diverse experience also includes being a teaching assistant, a waitress, a cashier, and a tutor."
             type="Paragraph"
-            className="w-96"
+            className="w-96 tablet:w-[600px] mobile:w-full"
           />
         </div>
-        <div className="flex gap-20 mobile:gap-0">
-          <div className="border-solid border-y-0 border-r-0 border-gray-300 border-4 pl-20 mobile:border-none mobile:pl-0">
+        <div className="flex tablet:gap-0">
+          <div className="border-solid border-y-0 border-r-0 border-gray-300 border-4 pl-16 pr-10 tablet:border-none mobile:pl-5">
             <Text text="Skills" type="Heading2" />
-            <div className="flex gap-10 font-poppins text-md text-navy">
+            <div className="flex gap-12">
               <ul className="list-disc">
-                <li>Typescript</li>
-                <li>HTML</li>
-                <li>CSS</li>
-                <li>JavaScript</li>
-                <li>Python</li>
-                <li>Java</li>
-                <li>Kotlin</li>
+                {skills1.map((item, index) => (
+                  <li key={index}>
+                    <Text type="Paragraph" text={item} />
+                  </li>
+                ))}
               </ul>
               <ul className="list-disc">
-                <li>Jira</li>
-                <li>Git/Github</li>
-                <li>Confluence</li>
-                <li>Figma</li>
-                <li>React</li>
-                <li>Tailwind CSS</li>
-                <li>ESLint</li>
+                {skills2.map((item, index) => (
+                  <li key={index}>
+                    <Text type="Paragraph" text={item} />
+                  </li>
+                ))}
               </ul>
             </div>
           </div>
-          <div className="border-solid border-y-0 border-r-0 border-gray-300 border-4 pl-20 mobile:border-none mobile:pl-14">
+          <div className="border-solid border-y-0 border-r-0 border-gray-300 border-4 pl-12 tablet:border-none tablet:pl-14 mobile:pl-0">
             <Text text="Links" type="Heading2" className="mb-4" />
             <div className="flex flex-col gap-5">
               <a
@@ -112,50 +127,59 @@ function Home() {
           </div>
         </div>
       </div>
-      <div className="flex flex-col gap-4 w-[1200px] mobile:w-full mobile:px-4">
-        <Text text="My Co-op in Korea" type="Heading2" />
-        <Text text="July-December 2024" type="Paragraph" className="italic" />
-        <div className="grid grid-cols-3 grid-rows-2 gap-3 mobile:flex mobile:flex-col">
-          <Text
-            text="I had the wonderful opportunity to have my first co-op in Seoul, South Korea as a front-end engineer at  Globaleur . Globaleur is a company that connects travelers to their destinations for a personalized travel experience. I contributed to Globaleur's white label product by working on tour pages, points of interests pages, and city pages."
-            type="Paragraph"
-            className="flex items-center"
-          />
-          <Text
-            text="I also worked on individual components such as the navigation bar, footer, and two-step gallery view! I deepened my knowledge in React, Typescript, and Tailwind CSS. The most rewarding experience was brainstorming a product with the team and seeing it come to life through implementation. "
-            type="Paragraph"
-            className="flex items-center"
-          />
-          <div className="flex flex-col gap-4 justify-center">
+      <div className="flex flex-col gap-4 w-[1023px] tablet:w-full tablet:px-4">
+        <div>
+          <Text text="My Co-op in Korea" type="Heading2" />
+          <a href="https://www.globaleur.com/" className="underline">
+            <Text text="Globaleur" type="Paragraph" />
+          </a>
+          <Text text="July-December 2024" type="Paragraph" className="italic" />
+        </div>
+        <div className="grid grid-cols-3 grid-rows-1 gap-3 tablet:flex tablet:flex-col">
+          <div className="flex flex-col gap-4 justify-between tablet:grid tablet:grid-cols-2 mobile:flex mobile:flex-col">
             <Text
-              text="Through this global co-op, I was able to do plenty of traveling and exploring! I traveled to Busan, Japan, and Jeju Island, all of which I recommend taking a trip to (especially Jeju Island)."
+              text="I had the wonderful opportunity to have my first co-op in Seoul, South Korea as a front-end engineer at  Globaleur . Globaleur is a company that connects travelers to their destinations for a personalized travel experience. I contributed to Globaleur's white label product by working on tour pages, points of interests pages, and city pages."
               type="Paragraph"
+              className="flex items-center"
             />
-            <Text
-              text="Exploring Asia and working with such talented people was an unforgettable experience."
-              type="Paragraph"
+            <Image
+              src={globaleurEngineeringTeam}
+              alt="Kaylie and the Gloableur Engineering Team"
+              caption="Globaleur Engineering Team"
             />
           </div>
-          <Image
-            src={globaleurEngineeringTeam}
-            alt="Kaylie and the Gloableur Engineering Team"
-            caption="Globaleur Engineering Team"
-            className="float-left"
-          />
-          <Image
-            src={globaleurManager}
-            alt="Kaylie and her manager"
-            caption="Daniel (my manager) and I"
-            className=""
-          />
-          <Image
-            src={hallasan}
-            alt="View from the top of Hallasan"
-            caption="View from the top of Hallasan"
-          />
+          <div className="flex flex-col gap-4 justify-between tablet:grid tablet:grid-cols-2 mobile:flex mobile:flex-col">
+            <Text
+              text="I also worked on individual components such as the navigation bar, footer, and two-step gallery view! I deepened my knowledge in React, Typescript, and Tailwind CSS. The most rewarding experience was brainstorming a product with the team and seeing it come to life through implementation. "
+              type="Paragraph"
+              className="flex items-center"
+            />
+            <Image
+              src={globaleurManager}
+              alt="Kaylie and her manager"
+              caption="Daniel (my manager) and I"
+            />
+          </div>
+          <div className="flex flex-col gap-4 justify-between tablet:grid tablet:grid-cols-2 mobile:flex mobile:flex-col">
+            <div className="flex flex-col justify-center gap-4">
+              <Text
+                text="Through this global co-op, I was able to do plenty of traveling and exploring! I traveled to Busan, Japan, and Jeju Island, all of which I recommend taking a trip to (especially Jeju Island)."
+                type="Paragraph"
+              />
+              <Text
+                text="Exploring Asia and working with such talented people was an unforgettable experience."
+                type="Paragraph"
+              />
+            </div>
+            <Image
+              src={hallasan}
+              alt="View from the top of Hallasan"
+              caption="View from the top of Hallasan"
+            />
+          </div>
         </div>
       </div>
-      <div className="mobile:px-4">
+      <div className="tablet:px-4">
         <Text text="My Projects" type="Heading2" className="flex mb-5" />
         <Card
           img={reversiGame}
@@ -165,7 +189,7 @@ function Home() {
               pattern, Java Swing GUI, and MVC framework following SOLID
               principles, tested with JUnit."
           headerFirst={false}
-          className="w-[1000px] mb-5 h-[250px] mobile:w-full"
+          className="w-[1000px] mb-5 h-[250px] tablet:w-full"
         />
         <Card
           img={solitaireGame}
@@ -175,7 +199,7 @@ function Home() {
               user-friendly text commands, and adhered to SOLID principles
               using MVC, tested with JUnit."
           headerFirst={false}
-          className="w-[1000px] h-[250px] mobile:w-full"
+          className="w-[1000px] h-[250px] tablet:w-full"
         />
       </div>
       <Text
